4331c627209d5636c0947848b106017f3690e9f5
Sswastik60 <sswastik60@gmail.com>
2026-07-18 23:19:11 +0530

    Task 3: Completed the code for getting the book details based on the author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -28,7 +28,14 @@ public_users.get('/isbn/:isbn',function (req, res) {
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let author = req.params.author;
+  let booksByAuthor = [];
+  for (let isbn in books) {
+    if (books[isbn].author === author) {
+      booksByAuthor.push(books[isbn]);
+    }
+  }
+  return res.status(200).json({books: booksByAuthor});
 });
 
 // Get all books based on title
