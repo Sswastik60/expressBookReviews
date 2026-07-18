47dcb0009607c4016b47ac35a29ac25217a7167d
Sswastik60 <sswastik60@gmail.com>
2026-07-18 23:41:04 +0530

    Task 5 : Completed code to access book review using ISBN number

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -52,8 +52,11 @@ public_users.get('/title/:title',function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let isbn = req.params.isbn;
+  if(books[isbn]){
+    return res.status(200).json(books[isbn].reviews);
+  }
+  return res.status(404).json({message: "Book not found"});
 });
 
 module.exports.general = public_users;
